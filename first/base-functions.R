027b5f47c68d0509b19e2aaf65a57e079edf7505
veronica948 <29.veronica.948@gmail.com>
2016-02-23 19:49:20 +0300

    Add base

---
+++ first/base-functions.R
@@ -0,0 +1,15 @@
+#with default step = 1
+arr = 1:12
+arr[5]
+# with step = 0.25
+s = seq(1,2,0.25)
+s[3]
+#list
+list1 = c(1,3,8)
+list1[1]
+#average
+mean(arr)
+sd(arr)
+var(arr)
+median(seq(1,2,0.25))
+
